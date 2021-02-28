2935f06ffce67a2ed2ea64f8b9641001a9601cba
dimple000 <58557175+dimple000@users.noreply.github.com>
2021-02-28 20:25:55 +0900

    Update index.py

--- index.py
+++ index.py
@@ -8,26 +8,8 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("봇이 성공적으로 실행되었습니다.")
-    game = discord.Game('청소중')
+    game = discord.Game('')
     await client.change_presence(status=discord.Status.online, activity=game)
-
-
-@client.event
-async def on_message(message):
-    if message.content.startswith('!청소'):
-        try:
-            if message.author.guild_permissions.manage_messages:
-                amount = message.content[4:]
-                await message.delete()
-                await message.channel.purge(limit=int(amount))
-                message = await message.channel.send(embed=discord.Embed(title='🧹 메시지 ' + str(amount) + '개 삭제됨', colour=discord.Colour.green()))
-                await asyncio.sleep(2)
-                await message.delete()
-            else:
-                message = await message.channel.send(embed=discord.Embed(title='🔑 명령어 사용권한이 없습니다. ')),
-        except:
-            pass
-
 
 @client.event
 async def on_message_delete(message):
